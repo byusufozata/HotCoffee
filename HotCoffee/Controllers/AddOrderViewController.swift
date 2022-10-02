0184bde2c5f3bd4cc48005cfae11abb8caa1edd8
YUSUF ÖZATA <byusufozata@gmail.com>
2022-10-02 16:15:18 +0300

    Initial Commit

---
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddOrderViewController.swift
+//  HotCoffee
+//
+//  Created by YUSUF ÖZATA on 2.10.2022.
+//
+
+import UIKit
+
+class AddOrderViewController: UIViewController{
+     
+}
---
+++ HotCoffee/Controllers/OrderTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrderTableViewController.swift
+//  HotCoffee
+//
+//  Created by YUSUF ÖZATA on 2.10.2022.
+//
+
+import Foundation
+import UIKit
+
+class OrdersTableViewController : UITableViewController{
+    
+}
